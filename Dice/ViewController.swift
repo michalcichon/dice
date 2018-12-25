60279ef790558ac4beed1a4f5ffbfc6b23c96b50
Michał Cichoń <michal.cichon@codete.com>
2018-12-25 18:23:36 +0100

    Initial Commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Michał Cichoń on 25/12/2018.
+//  Copyright © 2018 Michal Cichon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
